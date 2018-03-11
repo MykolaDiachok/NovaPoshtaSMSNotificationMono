36f61c51c3dd9fe80b0ff2bd49fbfff5d865c75e
Mykola Diachok <ndyachok@gmail.com>
2018-03-11 18:46:05 +0200

    Add project files.

---
+++ NovaPoshtaSMSNotification/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace NovaPoshtaSMSNotification
+{
+    [Activity(Label = "NovaPoshtaSMSNotification", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
